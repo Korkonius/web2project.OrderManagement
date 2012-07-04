9ff97f5da56d3956e947b9fb578869f24906f7f3
Eirik Ottesen <Korkonius@gmail.com>
2012-07-04 14:30:00 +0200

    Added missing fields to the component edit list

--- components.js
+++ components.js
@@ -3,7 +3,8 @@ $(document).ready(function(){
         componentName: '?m=ordermgmt&a=cedit&field=name&cid=',
         componentNumber: '?m=ordermgmt&a=cedit&field=catalog_nr&cid=',
         componentSupplier: '?m=ordermgmt&a=cedit&field=supplier&cid=',
-        componentPrice: '?m=ordermgmt&a=cedit&field=name&cid=',
+        componentVPrice: '?m=ordermgmt&a=cedit&field=vprice&cid=',
+        componentDiscount: '?m=ordermgmt&a=cedit&field=discount&cid='
     }, {
         animate: true
     });
--- components.php
+++ components.php
@@ -10,6 +10,17 @@ require_once(dirname(__FILE__) . '/lib/tbs_class.php');
 require_once(dirname(__FILE__) . '/order.class.php');
 require_once(dirname(__FILE__) . '/orderpdf.class.php');
 
+// Check ACL to see if the user is allowed to view items in the order module
+if (!$acl->checkModule('ordermgmt', 'view')) {
+    $AppUI->setMsg("Access denied: Insufficient privilegies to view order list", UI_MSG_ERROR);
+    $AppUI->redirect('index.php');
+}
+
+// Output titleblock
+$titleBlock = new w2p_Theme_TitleBlock('Order Management :: Stored Components', 'folder5.png', $m, "$m.$a");
+$titleBlock->addCrumb("?m=ordermgmt", "To Order List");
+$titleBlock->show();
+
 // Get component listing template
 $tbs = & new clsTinyButStrong();
 $tbs->LoadTemplate(dirname(__FILE__) . "/templates/component_list.html");
