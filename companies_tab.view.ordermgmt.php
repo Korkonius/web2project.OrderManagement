bfc1a677200e71dd619d73584537067470928c1a
Eirik Ottesen <Korkonius@gmail.com>
2012-07-10 09:22:00 +0200

    Implemented view for orders related to a company

---
+++ companies_tab.view.ordermgmt.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once(dirname(__FILE__) . '/lib/tbs_class.php');
+require_once(dirname(__FILE__) . '/classes/order.class.php');
+
+$filter = array(
+    "company" => w2PgetParam($_GET, 'company_id')
+);
+$orders = COrder::createListFromDatabase(0, 2000, $filter);
+
+// Create template object
+$tbs = & new clsTinyButStrong();
+$tbs->LoadTemplate(dirname(__FILE__) . '/templates/order_list.html');
+$tbs->MergeBlock('order', $orders);
+$tbs->Show(TBS_OUTPUT);
